29368c534b41707aa80dc6f099534ca6c50941b8
Artem <artem.tumanov.93@inbox.ru>
2021-03-05 15:22:13 +0300

    add adaptive slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,33 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                    adaptiveHeight: true
+                }
+            }
+            // {
+            // breakpoint: 600,
+            // settings: {
+            //     slidesToShow: 2,
+            //     slidesToScroll: 2
+            // }
+            // },
+            // {
+            // breakpoint: 480,
+            // settings: {
+            //     slidesToShow: 1,
+            //     slidesToScroll: 1
+            // }
+            // }
+        ]
     });
   });
